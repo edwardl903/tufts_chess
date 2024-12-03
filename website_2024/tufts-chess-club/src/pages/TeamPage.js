79a2a13b922f1b5c084e19bcff8d89308cf13ef7
Edward Lai <edwardlai2003@gmail.com>
2024-12-03 15:29:27 -0500

    organization and domain stuff

--- website_2024/tufts-chess-club/src/pages/TeamPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const TeamPage = () => {
-        return (
-            <img src="/images/TeamPage.png" alt="IDK" className="full-screen-image" />
-        );
-      };
-      
-export default TeamPage;
